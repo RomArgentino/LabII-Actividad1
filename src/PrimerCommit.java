71c2dd988676c30b75e39c0c1fb861c0a58de1f3
RomArgentino <78028834+RomArgentino@users.noreply.github.com>
2021-08-24 13:03:51 -0300

    4to desafío

--- src/PrimerCommit.java
+++ src/PrimerCommit.java
@@ -1,2 +1,14 @@
 public class PrimerCommit {
+
+    public static void main (String args[]){
+
+        String nombre = "Mauricio Carlos Fabián Romero";
+        int edad = 32;
+        float salario = 20000;
+        boolean carnet = true;
+
+        
+
+    }
+
 }
